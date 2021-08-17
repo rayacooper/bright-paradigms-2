0374468d57c213ff14cca5b7269c6bdc8840ec7a
unknown <sarah.jkr@gmail.com>
2021-08-17 11:53:20 -0600

    Touchups for Relaunch

--- src/Routes.js
+++ src/Routes.js
@@ -3,16 +3,16 @@ import {Switch, Route} from 'react-router-dom'
  
 import About from './Components/About/About';
 import HomePage from './Components/HomePage/HomePage';
-import Projects from './Components/Projects/Projects';
+// import Projects from './Components/Projects/Projects';
 import WhoWeAre from './Components/WhoWeAre/WhoWeAre';
 import Videos from './Components/WhoWeAre/Videos'
-import ProjectX from './Components/Projects/AllProjects/Project X';
+// import ProjectX from './Components/Projects/AllProjects/Project X';
  
 export default (
     <Switch>
         <Route component={About} path='/about'/>
-        <Route component={ProjectX} path='/portfolio/projectx'/>
-        <Route component={Projects} path='/portfolio'/>
+        {/* <Route component={ProjectX} path='/portfolio/projectx'/> */}
+        {/* <Route component={Projects} path='/portfolio'/> */}
         <Route component={Videos} path='/whoweare/videos'/>
         <Route component={WhoWeAre} path='/whoweare'/>
         <Route component={HomePage} path='/'/>
